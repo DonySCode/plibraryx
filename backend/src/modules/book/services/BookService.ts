3da4b186a0345cd536ce66b7b43bb54e40037c20
DonySCode <donycastro80@gmail.com>
2024-11-24 11:30:09 -0400

    feat: Add missing book services

--- backend/src/modules/book/services/BookService.ts
+++ backend/src/modules/book/services/BookService.ts
@@ -1,15 +1,42 @@
 import {BookRepository} from "../repositories/BookRepository";
-import Book from "../models/Book";
+import {IGetBookOptions} from "../interfaces/IGetBookOptions";
+import {IBookCreationAttributes} from "../interfaces/IBookCreationAttributes";
+import {UUID} from "crypto";
+import {IBook} from "../interfaces/IBook";
 
 export class BookService {
-    private  bookRepository: BookRepository;
+    private bookRepository: BookRepository;
 
     constructor() {
         this.bookRepository = new BookRepository();
     }
 
-    public async getAll(): Promise<Book[]>{
-        return await this.bookRepository.getAll();
+    public async getAll(options: IGetBookOptions) {
+        return await this.bookRepository.getAll(options);
+    }
+
+    public async getById(id: UUID){
+        return await this.bookRepository.getById(id);
+    }
+
+    public async add(bookData: IBookCreationAttributes) {
+        return await this.bookRepository.add(bookData);
+    }
+
+    public async update(bookData: IBook){
+        return await this.bookRepository.update(bookData);
+    }
+
+    public async delete(id: UUID){
+        return await this.bookRepository.delete(id);
+    }
+
+    public async maskAsFavorite(id: UUID){
+        return await this.bookRepository.maskAsFavorite(id);
+    }
+
+    public async unmaskAsFavorite(id: UUID){
+        return await this.bookRepository.unmarkAsFavorite(id);
     }
 
 }
