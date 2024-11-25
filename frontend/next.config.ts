70bde34857d7dee608565a7a46ed682d1e809516
DonySCode <donycastro80@gmail.com>
2024-11-24 23:01:21 -0400

    feat: Centralize package json scripts

---
+++ frontend/next.config.ts
@@ -0,0 +1,15 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'example.com',
+
+            }
+        ],
+    },
+};
+
+export default nextConfig;
